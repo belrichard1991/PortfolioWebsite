29710875370c070f61ce5571dd5afa8f05821552
Bel Richard <lagodbel@gmail.com>
2025-05-08 22:40:51 +0800

    Add public and views HTML files

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ app.get('/contact', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/contact.html'));
 });
 
+app.use((req, res) => {
+  res.status(404).send('404 - Page not found');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
